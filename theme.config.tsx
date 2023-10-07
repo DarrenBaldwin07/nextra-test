ddf64e9faa3fc1e82ae803501829c034f11e12d2
Darren B <68653294+Devd0@users.noreply.github.com>
2023-10-06 21:59:14 -0400

    default white

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,9 @@ import { DocsThemeConfig } from "nextra-theme-docs"
 
 const config: DocsThemeConfig = {
   logo: <img src="/title-logo.svg" width={150} alt="tembo" />,
+  nextThemes: {
+    defaultTheme: "light",
+  },
   darkMode: false,
   project: {
     link: "https://tembo.io",
